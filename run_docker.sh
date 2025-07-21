d6e8a207aaa743c9fc6d3f608f3d9193bdbb68d8
Sambhav Jain <sambhav.jain@amd.com>
2025-07-21 23:10:36 +0000

    mount home dir instead

--- run_docker.sh
+++ run_docker.sh
@@ -8,23 +8,10 @@ docker build -f docker/Dockerfile \
              --build-arg UID=$(id -u) \
              .
 
-# If the target directory being volume mounted does not exist
-# on the host, Docker will create it and that new directory will
-# be owned by root by default, causing permission issues in the
-# container. To mitigate this, create it on the host as non-root
-# user to ensure correct permissions inside the container.
-mkdir -p "${HOME}/.venv"
-mkdir -p "${HOME}/.cache"
-
 # Bind mounts for the following:
 # - current directory to /src in the container
-# - virtual environment and cache directories
-# - user's bash configuration files
+# - user's HOME directory (useful for .bash*, .gitconfig, .venv, .cache etc)
 docker run -it \
            -v "$(pwd)":"/src" \
-           -v "${HOME}/.venv":"${HOME}/.venv" \
-           -v "${HOME}/.cache":"${HOME}/.cache" \
-           -v "${HOME}/.bashrc":"${HOME}/.bashrc" \
-           -v "${HOME}/.bash_aliases":"${HOME}/.bash_aliases" \
-           -v "${HOME}/.bash_profile":"${HOME}/.bash_profile" \
+           -v "${HOME}":"${HOME}" \
            main:dev
