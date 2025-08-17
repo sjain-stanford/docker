a271b03e288b9ea70d0ec754c01d529e278fb919
AaronStGeorge <aaronstgeorge@gmail.com>
2025-08-17 14:18:15 +0000

    Set up to run on AMD GPUs

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Get the directory of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
@@ -10,6 +12,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # - current directory to same dir in the container
 # - user's HOME directory (useful for .bash*, .gitconfig, .cache etc)
 docker run -it \
+           --device /dev/kfd \
+           --device /dev/dri \
+           --security-opt seccomp=unconfined \
            -v "${PWD}":"${PWD}" \
            -v "${HOME}":"${HOME}" \
            ubuntu-24.04-dev:latest
