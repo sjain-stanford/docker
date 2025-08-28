2e72226577bf4d46168edb778490b6769aeae7f4
Sambhav Jain <sambhav.jain@amd.com>
2025-08-28 20:48:03 +0000

    try security-opt

--- run_docker.sh
+++ run_docker.sh
@@ -12,6 +12,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker run -it \
            $( [ -e /dev/kfd ] && echo "--device /dev/kfd" ) \
            $( [ -e /dev/dri ] && echo "--device /dev/dri" ) \
+           --security-opt seccomp=unconfined \
            -v "${PWD}":"${PWD}" \
            -v "${HOME}":"${HOME}" \
            ubuntu-24.04-dev:latest
