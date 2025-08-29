acaea1b6ccb5a89442af6f3c01fd887944a391cc
Sambhav Jain <sambhav.jain@amd.com>
2025-08-29 04:34:06 +0000

    minor

--- exec_docker.sh
+++ exec_docker.sh
@@ -6,17 +6,18 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Build docker image if not already built
 "${SCRIPT_DIR}/build_docker.sh"
 
-# ROCm requires accesses to the host’s /dev/kfd and /dev/dri/* device nodes owned
-# by the `render` and `video` groups. The groups’ GIDs in the container must match
-# the host’s to access the resources. Sometimes the device nodes may be owned by
+# ROCm requires accesses to the host’s /dev/kfd and /dev/dri/* device nodes, typically
+# owned by the `render` and `video` groups. The groups’ GIDs in the container must
+# match the host’s to access the resources. Sometimes the device nodes may be owned by
 # dynamic GIDs (that don't belong to the `render` or `video` groups). So instead of
-# adding user to the named groups (using `getent group render` or `getent group video`)
-# we simply check the owning GID of the devices and pass it to the `docker run`.
+# adding user to the GIDs of named groups (obtained from `getent group render` or
+# `getent group video`), we simply check the owning GID of the device nodes on the host
+# and pass it to `docker run` with `--group-add=<GID>`.
 for DEV in /dev/kfd /dev/dri/*; do
   # Skip if not a character device
   # /dev/dri/by-path/ symlinks are ignored
-  [[ -c "$DEV" ]] || continue
-  DEVICE_GROUP_OPTS+=" --device=$DEV --group-add=$(stat -c '%g' $DEV)"
+  [[ -c "${DEV}" ]] || continue
+  DEVICE_GROUP_OPTS+=" --device=${DEV} --group-add=$(stat -c '%g' ${DEV})"
 done
 
 # Bind mounts for the following:
