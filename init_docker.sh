68367276bd7c7041233c9d327f5fdb9dbee0f840
Sambhav Jain <sambhav.jain@amd.com>
2025-09-06 14:50:30 +0000

    init_docker

---
+++ init_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# ROCm requires accesses to the host’s /dev/kfd and /dev/dri/* device nodes, typically
+# owned by the `render` and `video` groups. The groups’ GIDs in the container must
+# match the host’s to access the resources. Sometimes the device nodes may be owned by
+# dynamic GIDs (that don't belong to the `render` or `video` groups). So instead of
+# adding user to the GIDs of named groups (obtained from `getent group render` or
+# `getent group video`), we simply check the owning GID of the device nodes on the host
+# and pass it to `docker run` with `--group-add=<GID>`.
+for DEV in /dev/kfd /dev/dri/*; do
+  # Skip if not a character device
+  # /dev/dri/by-path/ symlinks are ignored
+  [[ -c "${DEV}" ]] || continue
+  DOCKER_RUN_DEVICE_OPTS+=" --device=${DEV} --group-add=$(stat -c '%g' ${DEV})"
+done
+
+# Export for use by `run_docker.sh` and `exec_docker.sh`
+export DOCKER_RUN_DEVICE_OPTS
