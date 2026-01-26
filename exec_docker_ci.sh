817f6fceba5a6f24add64056446ea75ff92de245
Praveen G <praveen.g2@amd.com>
2026-01-26 20:00:41 +0000

    Remove test code

--- exec_docker_ci.sh
+++ exec_docker_ci.sh
@@ -9,7 +9,6 @@ source "${SCRIPT_DIR}/init_docker.sh"
 # Bind mounts for the following:
 # - current directory to /workspace in the container
 # https://rocm.docs.amd.com/projects/install-on-linux/en/latest/how-to/docker.html#accessing-gpus-in-containers
-chmod +x "$SCRIPT_DIR/entrypoint.sh"
 
 docker run --rm \
            -v "${PWD}":/workspace \
@@ -17,8 +16,6 @@ docker run --rm \
            -e IREE_GIT_TAG=${IREE_GIT_TAG} \
            -e THEROCK_GIT_TAG=${THEROCK_GIT_TAG} \
            -e AMD_ARCH=${AMD_ARCH} \
-           -v "$SCRIPT_DIR/entrypoint.sh:/entrypoint.sh" \
-           --entrypoint /entrypoint.sh \
            --security-opt seccomp=unconfined \
            ghcr.io/sjain-stanford/compiler-dev-ubuntu-24.04:main \
            "$@"
